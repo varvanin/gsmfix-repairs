9dfdbe4645f6e9829ad603d2f369536bcb2bc55d
Yevhen Varvanin <varvanin@outlook.com>
2024-12-08 22:55:00 +0100

    Update BASE_URL.php

--- server/config/BASE_URL.php
+++ server/config/BASE_URL.php
@@ -1,6 +1,6 @@
 <?php
 
 $BASE_URL = [
-    'DEVELOPMENT' => $_SERVER['DOCUMENT_ROOT'].'/horizon-ui-chakra-main', // (DON'T add `/` in the end, Example: www.domain.com/MyApp)
-    'PRODUCTION' => $_SERVER['DOCUMENT_ROOT'].'/projects/HomeRepairs', // (DON'T add `/` in the end, Example: www.domain.com/MyApp)
+    'DEVELOPMENT' => $_SERVER['DOCUMENT_ROOT'].'https://gsmfix-repairs.vercel.app', // (DON'T add `/` in the end, Example: www.domain.com/MyApp)
+    'PRODUCTION' => $_SERVER['DOCUMENT_ROOT'].'https://gsmfix-repairs.vercel.app', // (DON'T add `/` in the end, Example: www.domain.com/MyApp)
 ];
